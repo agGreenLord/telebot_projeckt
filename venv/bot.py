363c85fdf003c1a683be68e2b5ce6ab16e359067
Anton <a.g.local.mail@gmail.com>
2022-12-05 21:49:40 +0300

    add text

--- venv/bot.py
+++ venv/bot.py
@@ -5,15 +5,11 @@ import telebot
 TOKEN = "5827639233:AAFoJXxbXJ9oWtsL9nIuVbaOEK35apz79AI"
 logging.basicConfig(level=logging.INFO)
 
-
-# def telegram_bot(token):
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'hello')
+    bot.send_message(message.chat.id, "Бот позволяет оставить заявку на ремонт, диагностику электроники, бытовой техники, проводки. С чем работать будем?")
 
 bot.polling()
 
-# if __name__ == '__main__':
-#     telegram_bot(TOKEN)
