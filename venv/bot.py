6f7734a628b4c287dab7277cdd70b27da635ded5
Anton <a.g.local.mail@gmail.com>
2022-12-05 22:16:55 +0300

    добавил строчку "когда вам удобно чтобы с вами связались",

--- venv/bot.py
+++ venv/bot.py
@@ -9,7 +9,24 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Бот позволяет оставить заявку на ремонт, диагностику электроники, бытовой техники, проводки. С чем работать будем?")
+    bot.send_message(message.chat.id, """
+    Бот позволяет оставить заявку на ремонт, диагностику электроники, бытовой техники, проводки. С чем работать будем?
+    "Электроника" 
+    "Крупная бытовая техника" 
+    "Проводка""")
+
+@bot.message_handler(content_types=['text'])
+def send_massage(message):
+    if message.text.lower() == 'электроника':
+         bot.send_message(message.chat.id, 'Когда вам было бы удобно, чтобы с вами связался мастер?')
+    elif message.text.lower() == 'бытовая техника':
+        bot.send_message(message.chat.id, 'Когда вам было бы удобно, чтобы с вами связался мастер?')
+    elif message.text.lower() == 'проводка':
+        bot.send_message(message.chat.id, 'Когда вам было бы удобно, чтобы с вами связался мастер?')
+    else:
+        bot.send_message(message.chat.id, 'Я вас не понимаю, пожалуйста напишите один из предложенных вариантов')
+
+
 
 bot.polling()
 
