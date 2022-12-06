52b5dcfd86f80ff5dfc6d5db38c1dde9c2dc010f
Anton <a.g.local.mail@gmail.com>
2022-12-06 21:34:46 +0300

    добавил больше текста и сделал реакцию на кнопки

--- venv/bot.py
+++ venv/bot.py
@@ -18,28 +18,30 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, """
-        Бот позволяет оставить заявку на ремонт, диагностику электроники, бытовой техники, проводки. С чем работать будем?
-        "Электроника" 
-        "Крупная бытовая техника" 
-        "Проводка""")
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Электроника")
-    btn2 = types.KeyboardButton('Бытовая техника')
-    btn3 = types.KeyboardButton('Проводка')
-    markup.add(btn1, btn2, btn3)
-    bot.send_message(message.from_user.id, "Выберете что вам необходимо починить", reply_markup=markup)
+        Привет! Я бот, который позволяет оставить заявку мастеру на починку электроники, бытовой техники или проводки.
+        Вы можете:
+        /info - узнать информацию о мастере
+        /oder - оставить заявку на звонок мастера
+        """)
 
 
 @bot.message_handler(content_types=['text'])
-def send_massage(message):
-    if message.text.lower() == 'электроника':
-         bot.send_message(message.chat.id, 'Пожалуйста оставьте свой номер телефона, чтобы мастер мог с вами связаться')
+def get_text_mesage(message):
+    if message.text == '/oder':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Электроника")
+        btn2 = types.KeyboardButton('Бытовая техника')
+        btn3 = types.KeyboardButton('Проводка')
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(message.from_user.id, "Выберете что вам необходимо починить", reply_markup=markup)
+
+    elif message.text.lower() == 'электроника':
+        bot.send_message(message.from_user.id, 'Пожалуйста, оставьте свой номер телефона, чтобы мастер смог с вами связаться, узнать о проблеме и назначить удобное время врстечи')
     elif message.text.lower() == 'бытовая техника':
-        bot.send_message(message.chat.id, 'Пожалуйста оставьте свой номер телефона, чтобы мастер мог с вами связаться')
+        bot.send_message(message.from_user.id, 'Пожалуйста, оставьте свой номер телефона, чтобы мастер смог с вами связаться, узнать о проблеме и назначить удобное время врстечи')
     elif message.text.lower() == 'проводка':
-        bot.send_message(message.chat.id, 'Пожалуйста оставьте свой номер телефона, чтобы мастер мог с вами связаться')
-    else:
-        bot.send_message(message.chat.id, 'Я вас не понимаю, пожалуйста напишите один из предложенных вариантов')
+        bot.send_message(message.from_user.id, 'Пожалуйста, оставьте свой номер телефона, чтобы мастер смог с вами связаться, узнать о проблеме и назначить удобное время врстечи')
+
 
 
 
