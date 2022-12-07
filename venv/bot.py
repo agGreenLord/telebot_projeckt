6827cfefae84bedbc58e5fc51d1b6fdc631ad285
Anton <a.g.local.mail@gmail.com>
2022-12-07 21:28:13 +0300

    Сделал переключатель кнопок с "nfo/oder" при нажатии ODER переходит на кнопки: "электр/быт.техн./проводка"

--- venv/bot.py
+++ venv/bot.py
@@ -1,33 +1,22 @@
-
 import telebot
 from telebot import types
 
 TOKEN = "5827639233:AAFoJXxbXJ9oWtsL9nIuVbaOEK35apz79AI"
-
 bot = telebot.TeleBot(TOKEN)
 
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#     bot.send_message(message.chat.id, """
-#     Бот позволяет оставить заявку на ремонт, диагностику электроники, бытовой техники, проводки. С чем работать будем?
-#     "Электроника"
-#     "Крупная бытовая техника"
-#     "Проводка""")
-
-# Попытался сделать кнопку
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, """
-        Привет! Я бот, который позволяет оставить заявку мастеру на починку электроники, бытовой техники или проводки.
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton('Info')
+    btn2 = types.KeyboardButton('Oder')
+    markup.add(btn1, btn2)
+    bot.send_message(message.from_user.id, """Привет! Я бот, который позволяет оставить заявку мастеру на починку электроники, бытовой техники или проводки.
         Вы можете:
         /info - узнать информацию о мастере
-        /oder - оставить заявку на звонок мастера
-        """)
-
-
+        /oder - оставить заявку на звонок мастера""", reply_markup=markup)
 @bot.message_handler(content_types=['text'])
-def get_text_mesage(message):
-    if message.text == '/oder':
+def get_message(message):
+    if message.text.lower() == 'oder':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton("Электроника")
         btn2 = types.KeyboardButton('Бытовая техника')
@@ -35,15 +24,19 @@ def get_text_mesage(message):
         markup.add(btn1, btn2, btn3)
         bot.send_message(message.from_user.id, "Выберете что вам необходимо починить", reply_markup=markup)
 
-    elif message.text.lower() == 'электроника':
-        bot.send_message(message.from_user.id,
-                         'Пожалуйста, оставьте свой номер телефона, чтобы мастер смог с вами связаться, узнать о проблеме и назначить удобное время встречи')
-    elif message.text.lower() == 'бытовая техника':
-        bot.send_message(message.from_user.id,
-                         'Пожалуйста, оставьте свой номер телефона, чтобы мастер смог с вами связаться, узнать о проблеме и назначить удобное время встречи')
-    elif message.text.lower() == 'проводка':
-        bot.send_message(message.from_user.id,
-                         'Пожалуйста, оставьте свой номер телефона, чтобы мастер смог с вами связаться, узнать о проблеме и назначить удобное время встречи')
+        if message.text.lower() == 'электроника':
+            bot.send_message(message.from_user.id,
+                                'Пожалуйста, оставьте свой номер телефона, чтобы мастер смог с вами связаться, узнать о проблеме и назначить удобное время встречи')
+        elif message.text.lower() == 'бытовая техника':
+            bot.send_message(message.from_user.id,
+                                 'Пожалуйста, оставьте свой номер телефона, чтобы мастер смог с вами связаться, узнать о проблеме и назначить удобное время встречи')
+        elif message.text.lower() == 'проводка':
+            bot.send_message(message.from_user.id,
+                                 'Пожалуйста, оставьте свой номер телефона, чтобы мастер смог с вами связаться, узнать о проблеме и назначить удобное время встречи')
+
+
+
+
 
 
 bot.infinity_polling()
