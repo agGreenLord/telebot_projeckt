9c3946a5e2a160b843eaf8bc8515883a6954d2a3
likato <deviro@gmail.com>
2022-12-16 02:35:59 +0000

    feat: remove test

--- test-store.py
+++
@@ -1,9 +0,0 @@
-from mocked_data import STAGES
-from state import Store, ClientData, Record
-
-user_storage = Store()
-
-empty_client_data = ClientData('', '')
-# record = Record('123243', empty_client_data, STAGES[0])
-# user_storage.add_new_record(record)
-print(user_storage.storage)
